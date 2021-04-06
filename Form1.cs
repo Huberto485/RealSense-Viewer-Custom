67f67d8e228882c328564679cfc2294d9936f314
Huberto485 <thehubert.antczak@gmail.com>
2021-04-06 18:26:16 +0100

    first commit

---
+++ Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RealSense_Viewer_Custom
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
