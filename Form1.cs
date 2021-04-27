527b0f69b5d0ad7f60f9648608791c0a17a8e76b
Huberto485 <thehubert.antczak@gmail.com>
2021-04-28 00:10:52 +0100

    Minor changes to layout, added threading foundation for recording metadata

--- Form1.cs
+++ Form1.cs
@@ -18,6 +18,7 @@ namespace RealSense_Viewer_Custom
 
         //Workers that work on different threads
         private BackgroundWorker depthWorker = new BackgroundWorker();
+        private BackgroundWorker recordWorker = new BackgroundWorker();
 
         //Information about camera settings
         private float distance = 0;
@@ -29,6 +30,7 @@ namespace RealSense_Viewer_Custom
             InitializeComponent();
 
             InitializeDepthWorker();
+            InitializeRecordWorker();
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -39,7 +41,22 @@ namespace RealSense_Viewer_Custom
         /// <summary>
         /// DEPTH FUNCTIONALITY
         /// </summary>
-        
+
+        //Button for this thread.
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (depthWorker.IsBusy != true)
+            {
+                depthWorker.RunWorkerAsync();
+                buttonStart.Text = "Stop Cam";
+            }
+            else
+            {
+                depthWorker.CancelAsync();
+                buttonStart.Text = "Start Cam";
+            }
+        }
+
         private void InitializeDepthWorker()
         {
             //Initialize begin-work-finish parts for the worker thread.
@@ -99,12 +116,12 @@ namespace RealSense_Viewer_Custom
         private void depthWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
             //Check progress flags.
-            if(e.ProgressPercentage == 1)
+            if (e.ProgressPercentage == 1)
             {
                 //If flag is 1, output depth.
                 labelCameraInfo.Text = string.Format("Distance: {0:N3} meters", distance);
             }
-            else 
+            else
             {
                 //Else change output to none;
                 labelCameraInfo.Text = "Distance: ---";
@@ -135,51 +152,54 @@ namespace RealSense_Viewer_Custom
         /// TAKE PICTURE FUNCTIONALITY
         /// </summary>
 
-        private void buttonStart_Click(object sender, EventArgs e)
+        private void buttonPicture_Click()
         {
-            if(depthWorker.IsBusy != true)
-            {
-                depthWorker.RunWorkerAsync();
-                buttonStart.Text = "Stop Cam";
-            }
-            else
-            {
-                depthWorker.CancelAsync();
-                buttonStart.Text = "Start Cam";
-            }
+
         }
 
         /// <summary>
         /// RECORDING FUNCTIONALITY
         /// </summary>
 
-        private void buttonRecord_Click(object sender, EventArgs e)
+        private void buttonRecord_Click()
         {
-            string infoFull = "";
-
-            //Check whether a camera is connected.
-            if (camera.cameraConnected() == true)
+            if(recordWorker.IsBusy != true)
             {
-                labelConnect.Text = "Connected!";
-
-                //Get current machine time and add 10 to it.
-                DateTime timerEnd = DateTime.Now.AddSeconds(10);
-
-                //For the next 10 seconds execute this record function.
-                while (DateTime.Compare(DateTime.Now, timerEnd) <= 0)
-                {
-                    labelCameraInfo.Text = "";
-
-                    System.Diagnostics.Debug.WriteLine(DateTime.Now.ToString());
-
-                    labelCameraInfo.Text = infoFull;
-                }
+                recordWorker.RunWorkerAsync();
             }
             else
             {
-                labelConnect.Text = "Not connected!";
+                recordWorker.CancelAsync();
             }
         }
+
+        private void InitializeRecordWorker()
+        {
+            //Initialize begin-work-finish parts for the worker thread.
+            recordWorker.DoWork += new DoWorkEventHandler(recordWorker_DoWork);
+            recordWorker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(recordWorker_RunWorkerCompleted);
+            recordWorker.ProgressChanged += new ProgressChangedEventHandler(recordWorker_ProgressChanged);
+
+            //Worker actions: can be cancelled; report progress./
+            recordWorker.WorkerSupportsCancellation = true;
+            recordWorker.WorkerReportsProgress = true;
+        }
+
+        private void recordWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            
+        }
+
+        private void recordWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            
+        }
+
+        private void recordWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            
+        }
+
 
         /// <summary>
         /// LOADING FUNCTIONALITY
